6c1e7a4304ad3b6f999dc6e776105235b7d007ee
Sam Stevens <sam@ctrl-alt-sam.com>
2017-05-12 21:36:27 -0500

    max votes for ppl and judges

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,12 @@
 $(function(){
+  var teams = null;
+  var judges = null;
+  var config = null;
+  var messages = null;
+  
   var teamsRef = firebase.database().ref('teams');
   teamsRef.on('value', function(snapshot) {
-    var teams = snapshot.val();
+    teams = snapshot.val();
     $('#votes').html('');
     for(var t in teams){
       var tml = '<div class="vote-team">';
@@ -10,18 +15,25 @@ $(function(){
       tml += '</div>';
       $('#votes').append(tml);
     }
-    
   });
+  var judgeRef = firebase.database().ref('judges');
+  judgeRef.on('value',function(snapshot){
+    judges = snapshot.val();
+  });
+  var configRef = firebase.database().ref('config');
+  configRef.on('value',function(snapshot){
+    config = snapshot.val();
+  });
+  //For no we can assume ^ is finished first but may need some checks: if(teams && judges && config){ countVotes(); }
   
-  
-  var starCountRef = firebase.database().ref('messages');
-  starCountRef.on('value', function(snapshot) {
+  var messagesRef = firebase.database().ref('messages');
+  messagesRef.on('value', function(snapshot) {
+    console.log(teams,judges,config);
     var voters = {};
-    var judges = {};
     var votes = {};
     var _1stplace = "";
     
-    var messages = snapshot.val();
+    messages = snapshot.val();
     var msg_html = ""
     for(var k in messages){
       var teamName = messages[k].text.toLowerCase();
@@ -29,12 +41,26 @@ $(function(){
         _1stplace = teamName;
       }
       votes[teamName] = votes[teamName] || 0;
+      if(judges[messages[k].from]){
+        judges[messages[k].from].votes = judges[messages[k].from].votes || 0;
+        if(judges[messages[k].from].votes >= config.max_judge_votes){
+          continue;
+        }
+        judges[messages[k].from].votes += 1;
+      }else{
+        voters[messages[k].from] = voters[messages[k].from] || 0;
+        if(voters[messages[k].from] >= config.max_votes){
+          continue;
+        }
+        voters[messages[k].from] += 1;
+      }
+      
       votes[teamName] += 1
       if(votes[teamName] > votes[_1stplace]){
         _1stplace = teamName;
       }
     }
-    console.log(votes,_1stplace);
+    console.log(votes,_1stplace,voters);
     for(var v in votes){
       var vp = (votes[v]/votes[_1stplace])*100;
       $('#'+v).css('width',vp+'%');
